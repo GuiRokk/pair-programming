293862890a916fb59b00d3570835764852485eb5
Gui Roque <gui.roque@outlook.com.br>
2021-04-12 18:36:03 -0300

    Pair-Programming Concluido

--- lib/word_hunter.rb
+++ lib/word_hunter.rb
@@ -1,2 +1,18 @@
 class WordHunter
+	def self.find(matrix, words)
+		inputs = words.split
+		comb = matrix + matrix.transpose + inverte(matrix) + inverte(matrix.transpose)
+
+		comb.map do |word|
+			if inputs.include?(word.join)
+				word.join
+			end
+		end.compact
+	end
+
+	def self.inverte(array)
+		array.map do |f|
+			f.reverse
+		end
+	end
 end
--- spec/word_hunter_spec.rb
+++ spec/word_hunter_spec.rb
@@ -6,5 +6,14 @@ describe 'WordHunter' do
               ['J', 'U', 'S'],
               ['N', 'G', 'S']]
     words = 'BUG'
+    expect(WordHunter.find(matrix, words)).to include('BUG')
+  end
+
+  it 'Recebe uma matriz e lista, retorna apenas as palavras solicitas.' do
+    matrix = [['B', 'U', 'G'],
+              ['J', 'U', 'S'],
+              ['N', 'G', 'S']]
+    words = 'BUG'
+    expect(WordHunter.find(matrix, words)).to contain_exactly('BUG')
   end
 end
